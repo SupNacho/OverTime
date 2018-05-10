b2772ae23ed86172e319997aa7d07ac8f8778da1
Artem Shumilov <vanbellum@gmail.com>
2018-05-10 17:06:47 +0300

    added comments with time stamp feature and some restore timer state func

--- app/src/main/java/ru/supernacho/overtime/model/repository/TimerRepository.java
+++ app/src/main/java/ru/supernacho/overtime/model/repository/TimerRepository.java
@@ -3,25 +3,33 @@ package ru.supernacho.overtime.model.repository;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
-import java.util.Date;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import io.reactivex.Observable;
 import timber.log.Timber;
 
 public class TimerRepository {
     private Date zeroTime;
+    private StringBuilder sb;
 
     public TimerRepository() {
         zeroTime = new Date();
         zeroTime.setTime(0L);
+        sb = new StringBuilder();
     }
 
-    public void startOverTime() {
+    public void startOverTime(String comment) {
         ParseObject overtime = new ParseObject("OverTime");
+        String timeStamp = new SimpleDateFormat( "dd-MM-yy HH:mm:ss",Locale.US).format(new Date());
+        sb.setLength(0);
+        sb.append(timeStamp).append(" ").append(comment);
         overtime.put(ParseFileds.createdBy, ParseUser.getCurrentUser().getObjectId());
         overtime.put(ParseFileds.startDate, new Date());
         overtime.put(ParseFileds.stopDate, zeroTime);
-
-        overtime.put(ParseFileds.comment, "metrak");
+        overtime.put(ParseFileds.comment, sb.toString());
 
         overtime.saveEventually(e -> {
             if (e == null) {
@@ -32,7 +40,7 @@ public class TimerRepository {
         });
     }
 
-    public void stopOverTime() {
+    public void stopOverTime(String comment) {
         Date zeroTime = new Date();
         zeroTime.setTime(0L);
         ParseQuery<ParseObject> runningTime = ParseQuery.getQuery("OverTime");
@@ -40,10 +48,23 @@ public class TimerRepository {
         runningTime
                 .whereNotEqualTo(ParseFileds.startDate, null)
                 .whereEqualTo(ParseFileds.createdBy, ParseUser.getCurrentUser().getObjectId())
-                .whereEqualTo(ParseFileds.stopDate, zeroTime);
+                .whereEqualTo(ParseFileds.stopDate, zeroTime)
+                .orderByDescending(ParseFileds.createdAt);
 
         runningTime.getFirstInBackground((object, e) -> {
             if (e == null && object != null) {
+                String oldComment = (String) object.get(ParseFileds.comment);
+                if (!comment.equals(oldComment)){
+                    String timeStamp = new SimpleDateFormat( "dd-MM-yy HH:mm:ss",Locale.US).format(new Date());
+                    sb.setLength(0);
+                    sb
+                            .append(oldComment)
+                            .append("\n")
+                            .append(timeStamp)
+                            .append(" ")
+                            .append(comment);
+                    object.put(ParseFileds.comment, sb.toString());
+                }
                 object.put(ParseFileds.stopDate, new Date());
                 object.saveEventually(e1 -> {
                     if (e1 == null) {
@@ -55,5 +76,25 @@ public class TimerRepository {
             }
         });
 
+    }
+
+    public Observable<String> restoreTimerState(){
+        return Observable.create( emit -> {
+            final String[] startDate = new String[1];
+            ParseQuery<ParseObject> runningTime = ParseQuery.getQuery("OverTime");
+            runningTime
+                    .whereNotEqualTo(ParseFileds.startDate, null)
+                    .whereEqualTo(ParseFileds.createdBy, ParseUser.getCurrentUser().getObjectId())
+                    .whereEqualTo(ParseFileds.stopDate, zeroTime)
+                    .orderByDescending(ParseFileds.createdAt);
+
+            runningTime.getFirstInBackground((object, e) -> {
+                if (e == null && object != null) {
+                    startDate[0] = new SimpleDateFormat("HH:mm:ss", Locale.US)
+                            .format(object.get(ParseFileds.startDate));
+                    emit.onNext(startDate[0]);
+                }
+            });
+        });
     }
 }
