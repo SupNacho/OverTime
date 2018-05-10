323dd24f7784233a4c0644947a2d78ed6284ca3f
Artem Shumilov <vanbellum@gmail.com>
2018-05-10 17:05:07 +0300

    fixes for restore timer state on rotate device or relaunch app

--- app/src/main/java/ru/supernacho/overtime/view/fragments/TimerView.java
+++ app/src/main/java/ru/supernacho/overtime/view/fragments/TimerView.java
@@ -1,11 +1,12 @@
 package ru.supernacho.overtime.view.fragments;
 
 import com.arellomobile.mvp.MvpView;
-import com.arellomobile.mvp.viewstate.strategy.SingleStateStrategy;
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
-@StateStrategyType(value = SingleStateStrategy.class)
+@StateStrategyType(value = AddToEndSingleStrategy.class)
 public interface TimerView extends MvpView {
-    void setCounter(String countMsg);
+    void setCounter(String countMsg, int secs);
     void setStartDate(String startDate);
+    void setTimerState(boolean isStarted);
 }
