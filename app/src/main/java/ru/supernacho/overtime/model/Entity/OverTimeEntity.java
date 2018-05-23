b5b60d68052887b4ad537179e4cbcf892206aa68
Artem Shumilov <vanbellum@gmail.com>
2018-05-23 15:19:08 +0300

    Overtime entity added

--- app/src/main/java/ru/supernacho/overtime/model/Entity/OverTimeEntity.java
+++ app/src/main/java/ru/supernacho/overtime/model/Entity/OverTimeEntity.java
@@ -1,4 +1,48 @@
 package ru.supernacho.overtime.model.Entity;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class OverTimeEntity {
+    private final Date startDate;
+    private final Date stopDate;
+    private final long duration;
+    private final String comment;
+    private final String durationString;
+    private final String dateLabel;
+
+    public OverTimeEntity(Date startDate, Date stopDate, long duration, String comment) {
+        this.startDate = startDate;
+        this.stopDate = stopDate;
+        this.duration = duration;
+        this.comment = comment;
+        this.durationString = new SimpleDateFormat("HH:mm:ss", Locale.US).format(duration);
+        this.dateLabel = new SimpleDateFormat("dd MMM", Locale.US).format(startDate);
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getStopDate() {
+        return stopDate;
+    }
+
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public String getDurationString() {
+        return durationString;
+    }
+
+    public String getDateLabel() {
+        return dateLabel;
+    }
 }
