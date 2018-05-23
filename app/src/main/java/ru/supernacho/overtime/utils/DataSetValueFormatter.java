7e5bdf00720084e0511fbe1c3ef27052936ddff1
Artem Shumilov <vanbellum@gmail.com>
2018-05-23 15:27:09 +0300

    added Chart data set formatter for bars value representation

--- app/src/main/java/ru/supernacho/overtime/utils/DataSetValueFormatter.java
+++ app/src/main/java/ru/supernacho/overtime/utils/DataSetValueFormatter.java
@@ -1,4 +1,15 @@
 package ru.supernacho.overtime.utils;
 
-public class DataSetValueFormatter {
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+public class DataSetValueFormatter implements IValueFormatter {
+    @Override
+    public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
+        return new SimpleDateFormat("HH:mm", Locale.US).format((long)value);
+    }
 }
