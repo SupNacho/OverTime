2c36eea317f8f3dbaa18a78b795f1ff02acad863
Artem Shumilov <vanbellum@gmail.com>
2018-05-23 15:29:41 +0300

    added module for DI Chart repository

--- app/src/main/java/ru/supernacho/overtime/di/modules/ChartRepoModule.java
+++ app/src/main/java/ru/supernacho/overtime/di/modules/ChartRepoModule.java
@@ -1,4 +1,16 @@
 package ru.supernacho.overtime.di.modules;
 
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import ru.supernacho.overtime.model.repository.ChartRepository;
+
+@Singleton
+@Module
 public class ChartRepoModule {
+    @Provides
+    ChartRepository chartRepository(){
+        return new ChartRepository();
+    }
 }
