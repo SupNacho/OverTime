45b0777ff19cae63b9514d1d0d23712bb3e177b2
Artem Shumilov <vanbellum@gmail.com>
2018-05-23 15:32:43 +0300

    Added DI injection method

--- app/src/main/java/ru/supernacho/overtime/di/AppComponent.java
+++ app/src/main/java/ru/supernacho/overtime/di/AppComponent.java
@@ -4,11 +4,13 @@ import javax.inject.Singleton;
 
 import dagger.Component;
 import ru.supernacho.overtime.di.modules.AppModule;
+import ru.supernacho.overtime.di.modules.ChartRepoModule;
 import ru.supernacho.overtime.di.modules.LogRepoModule;
 import ru.supernacho.overtime.di.modules.LoginRepoModule;
 import ru.supernacho.overtime.di.modules.TimerRepoModule;
 import ru.supernacho.overtime.model.repository.LoginRepository;
 import ru.supernacho.overtime.model.repository.TimerRepository;
+import ru.supernacho.overtime.presenter.ChartPresenter;
 import ru.supernacho.overtime.presenter.DateChooserPresenter;
 import ru.supernacho.overtime.presenter.LoginPresenter;
 import ru.supernacho.overtime.presenter.TabsPresenter;
@@ -16,11 +18,13 @@ import ru.supernacho.overtime.presenter.TimerPresenter;
 import ru.supernacho.overtime.view.LoginActivity;
 
 @Singleton
-@Component(modules = {AppModule.class, LoginRepoModule.class, TimerRepoModule.class, LogRepoModule.class})
+@Component(modules = {AppModule.class, LoginRepoModule.class, TimerRepoModule.class, LogRepoModule.class,
+        ChartRepoModule.class})
 public interface AppComponent {
     void inject(LoginActivity activity);
     void inject(LoginPresenter presenter);
     void inject(TabsPresenter presenter);
     void inject(TimerPresenter presenter);
     void inject(DateChooserPresenter presenter);
+    void inject(ChartPresenter presenter);
 }
