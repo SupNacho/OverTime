630625565d22ad6792a97f7785fdd3b7f2ff7252
Artem Shumilov <vanbellum@gmail.com>
2018-05-23 15:27:53 +0300

    Chart Y Axis value formatter

--- app/src/main/java/ru/supernacho/overtime/utils/YAxisValueFormatter.java
+++ app/src/main/java/ru/supernacho/overtime/utils/YAxisValueFormatter.java
@@ -1,4 +1,14 @@
 package ru.supernacho.overtime.utils;
 
-public class YAxisValueFormatter {
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+public class YAxisValueFormatter implements IAxisValueFormatter {
+    @Override
+    public String getFormattedValue(float value, AxisBase axis) {
+        return new SimpleDateFormat("HH:mm", Locale.US).format((long) value);
+    }
 }
