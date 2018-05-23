c7ec05c195828e078aebfd1b15f0da91e25849a8
Artem Shumilov <vanbellum@gmail.com>
2018-05-23 15:31:16 +0300

    filled with code Chart fragment to its base functionality

--- app/src/main/java/ru/supernacho/overtime/view/fragments/ChartFragment.java
+++ app/src/main/java/ru/supernacho/overtime/view/fragments/ChartFragment.java
@@ -2,47 +2,80 @@ package ru.supernacho.overtime.view.fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.arellomobile.mvp.MvpAppCompatDialogFragment;
+import com.arellomobile.mvp.presenter.InjectPresenter;
+import com.arellomobile.mvp.presenter.ProvidePresenter;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import ru.supernacho.overtime.App;
 import ru.supernacho.overtime.R;
+import ru.supernacho.overtime.model.Entity.OverTimeEntity;
+import ru.supernacho.overtime.presenter.ChartPresenter;
+import ru.supernacho.overtime.utils.XAxisValuesFormatter;
+import ru.supernacho.overtime.utils.DataSetValueFormatter;
+import ru.supernacho.overtime.utils.YAxisValueFormatter;
+import timber.log.Timber;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ChartFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ChartFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class ChartFragment extends MvpAppCompatDialogFragment implements ChartView,
+        OnChartValueSelectedListener{
+    private static final String ARG_PARAM1 = "month";
+    private static final String ARG_PARAM2 = "year";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private int month;
+    private int year;
+    private List<OverTimeEntity> overTimesList;
+    private Unbinder unbinder;
+
+    @InjectPresenter
+    ChartPresenter presenter;
+
+    @BindView(R.id.bc_chart_fragment)
+    BarChart barChart;
+
+    @BindView(R.id.tv_comment_chart_fragment)
+    TextView tvComment;
+
+    private List<BarEntry> yVals = new ArrayList<>();
+    private List<String> labels = new ArrayList<>();
 
 
     public ChartFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChartFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ChartFragment newInstance(String param1, String param2) {
+    public static ChartFragment newInstance(int month, int year) {
         ChartFragment fragment = new ChartFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt(ARG_PARAM1, month);
+        args.putInt(ARG_PARAM2, year);
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,16 +84,77 @@ public class ChartFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            month = getArguments().getInt(ARG_PARAM1);
+            year = getArguments().getInt(ARG_PARAM2);
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chart, container, false);
+        View view = inflater.inflate(R.layout.fragment_chart, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        presenter.getOverTimes(month, year);
+        return view;
     }
 
+    @ProvidePresenter
+    public ChartPresenter providePresenter(){
+        ChartPresenter presenter = new ChartPresenter(AndroidSchedulers.mainThread());
+        App.getInstance().getAppComponent().inject(presenter);
+        return presenter;
+    }
+
+    @Override
+    public void updateChartView(List<OverTimeEntity> overTimeEntityList) {
+        overTimesList = overTimeEntityList;
+        labels.clear();
+        yVals.clear();
+        for (OverTimeEntity overTimeEntity : overTimeEntityList) {
+            labels.add(overTimeEntity.getDateLabel());
+            yVals.add(new BarEntry(overTimeEntityList.indexOf(overTimeEntity), overTimeEntity.getDuration()));
+        }
+        BarDataSet dataSet = new BarDataSet(yVals, "Hours per day");
+        dataSet.setValueFormatter(new DataSetValueFormatter());
+        dataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+        dataSet.setDrawValues(true);
+        BarData barData = new BarData(dataSet);
+        barData.setDrawValues(true);
+        barData.setValueTextSize(15f);
+        barData.setValueTextColor(R.color.colorAccent);
+        barData.setBarWidth(0.6f);
+        barChart.setMaxVisibleValueCount(31); //Max days in month
+        YAxis yAxisLeft = barChart.getAxisLeft();
+        yAxisLeft.setValueFormatter(new YAxisValueFormatter());
+        XAxis xAxisBarChart = barChart.getXAxis();
+        xAxisBarChart.setLabelCount(labels.size());
+        xAxisBarChart.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxisBarChart.setValueFormatter(new XAxisValuesFormatter(labels));
+        barChart.setData(barData);
+        barChart.getDescription().setEnabled(false);
+        barChart.getAxisRight().setEnabled(false);
+        barChart.setVisibleXRangeMinimum(1);
+        barChart.setVisibleXRangeMaximum(4);
+        barChart.setOnChartValueSelectedListener(this);
+        barChart.animateXY(1000,3000);
+        barChart.invalidate();
+    }
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+        tvComment.setText(overTimesList.get((int) e.getX()).getComment());
+    }
+
+    @Override
+    public void onNothingSelected() {
+        tvComment.setText("Select overtime to see comments about this work");
+    }
+
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
+    }
 }
