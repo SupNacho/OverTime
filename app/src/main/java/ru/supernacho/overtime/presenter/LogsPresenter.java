a80b3ccadce07d4184be6bf0a23033cf2303f536
Artem Shumilov <vanbellum@gmail.com>
2018-05-23 17:58:30 +0300

    moxy mvp interface

--- app/src/main/java/ru/supernacho/overtime/presenter/LogsPresenter.java
+++ app/src/main/java/ru/supernacho/overtime/presenter/LogsPresenter.java
@@ -1,4 +1,23 @@
 package ru.supernacho.overtime.presenter;
 
-public class LogsPresenter {
+import com.arellomobile.mvp.InjectViewState;
+import com.arellomobile.mvp.MvpPresenter;
+
+import ru.supernacho.overtime.view.fragments.LogsView;
+
+@InjectViewState
+public class LogsPresenter extends MvpPresenter<LogsView> {
+    @Override
+    protected void onFirstViewAttach() {
+        super.onFirstViewAttach();
+        getViewState().callDateChooser();
+    }
+
+    public void openChart(int month, int year){
+        getViewState().openChartFragment(month, year);
+    }
+
+    public void openDateChooser(){
+        getViewState().callDateChooser();
+    }
 }
--- app/src/main/java/ru/supernacho/overtime/view/fragments/LogsView.java
+++ app/src/main/java/ru/supernacho/overtime/view/fragments/LogsView.java
@@ -1,4 +1,11 @@
 package ru.supernacho.overtime.view.fragments;
 
-public class LogsView {
+import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.AddToEndStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
+
+@StateStrategyType(value = AddToEndStrategy.class)
+public interface LogsView extends MvpView{
+    void callDateChooser();
+    void openChartFragment(int month, int year);
 }
