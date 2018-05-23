44e7be95b9f54230268a1540d1a51dfae53303f2
Artem Shumilov <vanbellum@gmail.com>
2018-05-23 15:22:21 +0300

    added interface with fragment tag constants for Logs tab childs

--- app/src/main/java/ru/supernacho/overtime/view/fragments/FragmentTag.java
+++ app/src/main/java/ru/supernacho/overtime/view/fragments/FragmentTag.java
@@ -1,4 +1,6 @@
 package ru.supernacho.overtime.view.fragments;
 
 public interface FragmentTag {
+    String DATE_CHOOSER = "2db0401d-a3f7-4109-a65b-fceccc1d9eb6";
+    String WORKER_CHART = "22453933-f9e5-40f9-b49b-7b421a229428";
 }
