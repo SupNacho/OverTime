2a45b25fbda4169e23f1fdbb5029b7dd99c84e13
Artem Shumilov <vanbellum@gmail.com>
2018-05-26 13:45:03 +0300

    added field for time zone issues

--- app/src/main/java/ru/supernacho/overtime/model/repository/ChartRepository.java
+++ app/src/main/java/ru/supernacho/overtime/model/repository/ChartRepository.java
@@ -1,9 +1,8 @@
 package ru.supernacho.overtime.model.repository;
 
-import com.parse.FindCallback;
-import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -19,6 +18,7 @@ public class ChartRepository {
         return Observable.create( emit -> {
             ParseQuery<ParseObject> query = ParseQuery.getQuery(ParseClass.OVER_TIME);
             query
+                    .whereEqualTo(ParseFields.createdBy, ParseUser.getCurrentUser().getObjectId())
                     .whereEqualTo(ParseFields.monthNum, month)
                     .whereEqualTo(ParseFields.yearNum, year)
                     .findInBackground((objects, e) -> {
@@ -27,8 +27,9 @@ public class ChartRepository {
                     for (ParseObject object : objects) {
                         Date start = object.getDate(ParseFields.startDate);
                         Date stop = object.getDate(ParseFields.stopDate);
+                        String timeZoneID = object.getString(ParseFields.timeZoneID);
                         long duration = stop.getTime() - start.getTime();
-                        overTimesList.add(new OverTimeEntity(start, stop, duration,
+                        overTimesList.add(new OverTimeEntity(start, stop, timeZoneID, duration,
                                 object.getString(ParseFields.comment)));
                     }
                     emit.onNext(overTimesList);
