f140fd1f61c7bc8484057f842db0b987864859c1
Artem Shumilov <vanbellum@gmail.com>
2018-05-26 13:48:52 +0300

    changed time lib to joda time - for more easily manage time zones

--- app/src/main/java/ru/supernacho/overtime/model/Entity/OverTimeEntity.java
+++ app/src/main/java/ru/supernacho/overtime/model/Entity/OverTimeEntity.java
@@ -1,8 +1,14 @@
 package ru.supernacho.overtime.model.Entity;
 
-import java.text.SimpleDateFormat;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Locale;
+import java.util.TimeZone;
+
+import ru.supernacho.overtime.utils.DurationToStringConverter;
 
 public class OverTimeEntity {
     private final Date startDate;
@@ -14,15 +20,22 @@ public class OverTimeEntity {
     private final String startDateTimeLabel;
     private final String stopDateTimeLabel;
 
-    public OverTimeEntity(Date startDate, Date stopDate, long duration, String comment) {
+    public OverTimeEntity(Date startDate, Date stopDate, String timeZoneID, long duration, String comment) {
         this.startDate = startDate;
         this.stopDate = stopDate;
         this.duration = duration;
         this.comment = comment;
-        this.durationString = new SimpleDateFormat("HH:mm:ss", Locale.US).format(duration);
-        this.startDateLabel = new SimpleDateFormat("dd MMM", Locale.US).format(startDate);
-        this.startDateTimeLabel = new SimpleDateFormat("dd MMM HH:mm", Locale.US).format(startDate);
-        this.stopDateTimeLabel = new SimpleDateFormat("dd MMM HH:mm", Locale.US).format(stopDate);
+        DateTime sdt = new DateTime(startDate);
+        DateTime fdt = new DateTime(stopDate);
+        sdt.withZone(DateTimeZone.forID(timeZoneID));
+        fdt.withZone(DateTimeZone.forID(timeZoneID));
+        DateTimeFormatter startDateLabelFormatter = DateTimeFormat.forPattern("dd MMM");
+        DateTimeFormatter startDateTimeLabelFormatter = DateTimeFormat.forPattern("dd MMM HH:mm");
+        DateTimeFormatter stopDateTimeLabelFormatter = DateTimeFormat.forPattern("dd MMM HH:mm");
+        this.durationString = DurationToStringConverter.convert(duration);
+        this.startDateLabel = startDateLabelFormatter.print(sdt);
+        this.startDateTimeLabel = startDateTimeLabelFormatter.print(sdt);
+        this.stopDateTimeLabel = stopDateTimeLabelFormatter.print(fdt);
     }
 
     public Date getStartDate() {
