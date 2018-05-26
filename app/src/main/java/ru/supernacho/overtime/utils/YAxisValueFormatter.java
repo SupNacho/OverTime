f2e35dfb3bc53a0c512f84dbcf3dde08ae69a7c7
Artem Shumilov <vanbellum@gmail.com>
2018-05-26 13:52:16 +0300

    fixed YAxis values to remove timezone dependency for duration

--- app/src/main/java/ru/supernacho/overtime/utils/YAxisValueFormatter.java
+++ app/src/main/java/ru/supernacho/overtime/utils/YAxisValueFormatter.java
@@ -3,12 +3,9 @@ package ru.supernacho.overtime.utils;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 
-import java.text.SimpleDateFormat;
-import java.util.Locale;
-
 public class YAxisValueFormatter implements IAxisValueFormatter {
     @Override
     public String getFormattedValue(float value, AxisBase axis) {
-        return new SimpleDateFormat("HH:mm", Locale.US).format((long) value);
+        return DurationToStringConverter.convert((long) value);
     }
 }
