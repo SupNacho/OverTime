a9522822ed096ceca6c892545e78186d22d3d008
Artem Shumilov <vanbellum@gmail.com>
2018-05-26 13:46:43 +0300

    added filter condition for separate users records

--- app/src/main/java/ru/supernacho/overtime/model/repository/LogRepository.java
+++ app/src/main/java/ru/supernacho/overtime/model/repository/LogRepository.java
@@ -2,6 +2,8 @@ package ru.supernacho.overtime.model.repository;
 
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -17,6 +19,7 @@ public class LogRepository {
     public Observable<Object[]> getMonths(){
         return Observable.create(emit -> {
             ParseQuery<ParseObject> query = new ParseQuery<>("OverTime");
+            query.whereEqualTo(ParseFields.createdBy, ParseUser.getCurrentUser().getObjectId());
                 query.findInBackground((objects, e) -> {
                     if (objects != null && e == null){
                         set.clear();
