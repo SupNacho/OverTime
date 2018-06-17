8ea3f88c7e7b859a0198ab7d3a8bbf713dc7ee2b
Artem Shumilov <vanbellum@gmail.com>
2018-06-17 16:15:03 +0300

    next feature

---
+++ app/src/main/java/ru/supernacho/overtime/view/CompanyRegistrationActivity.java
@@ -0,0 +1,15 @@
+package ru.supernacho.overtime.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ru.supernacho.overtime.R;
+
+public class CompanyRegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_registration);
+    }
+}
