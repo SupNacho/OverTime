0c8dcdebe32a9018b2a57c171dfb6f63087b1e74
Artem Shumilov <vanbellum@gmail.com>
2018-06-17 18:56:27 +0300

    added pin generator for generation company employee and brigadier pins

--- app/src/main/java/ru/supernacho/overtime/utils/PinGenerator.java
+++ app/src/main/java/ru/supernacho/overtime/utils/PinGenerator.java
@@ -1,4 +1,22 @@
 package ru.supernacho.overtime.utils;
 
+import java.text.DecimalFormat;
+import java.util.Random;
+
 public class PinGenerator {
+    public static String getEmplPin(){
+        Random generator = new Random();
+        DecimalFormat formatter = new DecimalFormat("000000");
+        generator.setSeed(System.currentTimeMillis());
+        int pin = generator.nextInt(1000000) % 1000000;
+        return formatter.format(pin);
+    }
+
+    public static String getAdminPin(){
+        Random generator = new Random();
+        DecimalFormat formatter = new DecimalFormat("000000");
+        generator.setSeed(System.currentTimeMillis());
+        int pin = generator.nextInt(1000000) % 1000000;
+        return formatter.format(pin);
+    }
 }
