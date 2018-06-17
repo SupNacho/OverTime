35a346ce3b094a8f11ef2ae2a4a64ac8730e78cc
Artem Shumilov <vanbellum@gmail.com>
2018-06-17 18:58:37 +0300

    added Company repository - adding company to DB with checks for unique pins

--- app/src/main/java/ru/supernacho/overtime/model/repository/CompanyRepository.java
+++ app/src/main/java/ru/supernacho/overtime/model/repository/CompanyRepository.java
@@ -1,4 +1,63 @@
 package ru.supernacho.overtime.model.repository;
 
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
+
+import io.reactivex.Observable;
+import ru.supernacho.overtime.utils.PinGenerator;
+
 public class CompanyRepository {
+
+    public Observable<Boolean> registerCompany(String name, String address, String email, String phone, String chief){
+        return Observable.create( emit -> {
+
+                ParseObject company = new ParseObject(ParseClass.COMPANY);
+
+                company.put(ParseFields.companyName, name);
+                company.put(ParseFields.companyAddress, address);
+                company.put(ParseFields.companyEmail, email);
+                company.put(ParseFields.companyPhone, phone);
+                company.put(ParseFields.companyChief, chief);
+
+                for (;;) {
+                    String emplPin = PinGenerator.getEmplPin();
+                    if (checkPin(ParseFields.companyEmpPin, emplPin)) {
+                        company.put(ParseFields.companyEmpPin, emplPin);
+                        break;
+                    }
+                }
+
+                for (;;) {
+                    String adminPin = PinGenerator.getAdminPin();
+                    if (checkPin(ParseFields.companyAdminPin, adminPin)) {
+                        company.put(ParseFields.companyAdminPin, adminPin);
+                        break;
+                    }
+                }
+
+                company.saveEventually(e -> {
+                    if (e == null){
+                        emit.onNext(true);
+                    } else {
+                        emit.onNext(false);
+                    }
+                });
+        });
+    }
+
+    private boolean checkPin(String field, String pin){
+        List<ParseObject> list;
+        ParseQuery<ParseObject> checkPin = ParseQuery.getQuery(ParseClass.COMPANY);
+        try {
+            list = checkPin.whereEqualTo(field, pin)
+                    .find();
+            return list == null || list.size() <= 0;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
