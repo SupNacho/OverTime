7fd73f8fe3b7472c254d3df8c693858f955db408
Artem Shumilov <vanbellum@gmail.com>
2018-06-17 19:01:30 +0300

    added Company Registration Presenter

--- app/src/main/java/ru/supernacho/overtime/presenter/CompanyRegistrationPresenter.java
+++ app/src/main/java/ru/supernacho/overtime/presenter/CompanyRegistrationPresenter.java
@@ -1,4 +1,57 @@
 package ru.supernacho.overtime.presenter;
 
-public class CompanyRegistrationPresenter {
+import com.arellomobile.mvp.InjectViewState;
+import com.arellomobile.mvp.MvpPresenter;
+
+import javax.inject.Inject;
+
+import io.reactivex.Scheduler;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+import ru.supernacho.overtime.model.repository.CompanyRepository;
+import ru.supernacho.overtime.view.CompanyRegistrationView;
+
+@InjectViewState
+public class CompanyRegistrationPresenter extends MvpPresenter<CompanyRegistrationView> {
+    private Scheduler uiScheduler;
+    private DisposableObserver<Boolean> disposableObserver;
+
+    @Inject
+    CompanyRepository repository;
+
+    public CompanyRegistrationPresenter(Scheduler uiScheduler) {
+        this.uiScheduler = uiScheduler;
+    }
+
+    @Override
+    protected void onFirstViewAttach() {
+        super.onFirstViewAttach();
+        disposableObserver = new DisposableObserver<Boolean>() {
+            @Override
+            public void onNext(Boolean aBoolean) {
+                if (aBoolean) {
+                    getViewState().registrationSuccess();
+                } else {
+                    getViewState().registrationFail();
+                }
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+    }
+
+    public void registerCompany(String name, String address, String email, String phone, String chief){
+        repository.registerCompany(name,address,email,phone,chief)
+                .subscribeOn(Schedulers.io())
+                .observeOn(uiScheduler)
+                .subscribe(disposableObserver);
+    }
 }
