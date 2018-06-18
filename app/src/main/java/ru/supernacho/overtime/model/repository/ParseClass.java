75d7cfd547c5061d0fe9dbf9bf7563ab50d91723
Artem Shumilov <vanbellum@gmail.com>
2018-06-18 15:56:51 +0300

    added usersCompanies class for improve user management with many companies in platform

--- app/src/main/java/ru/supernacho/overtime/model/repository/ParseClass.java
+++ app/src/main/java/ru/supernacho/overtime/model/repository/ParseClass.java
@@ -3,5 +3,6 @@ package ru.supernacho.overtime.model.repository;
 public interface ParseClass {
     String OVER_TIME = "OverTime";
     String COMPANY = "Company";
+    String USER_COMPANIES = "UserCompanies";
     String PARSE_USER = "_User";
 }
