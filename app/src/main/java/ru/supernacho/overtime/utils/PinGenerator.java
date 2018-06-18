f5da21074251a1648ade165f7f52c8583f34984f
Artem Shumilov <vanbellum@gmail.com>
2018-06-18 15:55:35 +0300

    removed one unnecessary method

--- app/src/main/java/ru/supernacho/overtime/utils/PinGenerator.java
+++ app/src/main/java/ru/supernacho/overtime/utils/PinGenerator.java
@@ -4,15 +4,7 @@ import java.text.DecimalFormat;
 import java.util.Random;
 
 public class PinGenerator {
-    public static String getEmplPin(){
-        Random generator = new Random();
-        DecimalFormat formatter = new DecimalFormat("000000");
-        generator.setSeed(System.currentTimeMillis());
-        int pin = generator.nextInt(1000000) % 1000000;
-        return formatter.format(pin);
-    }
-
-    public static String getAdminPin(){
+    public static String getPin(){
         Random generator = new Random();
         DecimalFormat formatter = new DecimalFormat("000000");
         generator.setSeed(System.currentTimeMillis());
