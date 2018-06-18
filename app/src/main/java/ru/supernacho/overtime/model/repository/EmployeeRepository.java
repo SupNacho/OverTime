ba8c18bc13079c01881ac9e385dbdabaf46a0be6
Artem Shumilov <vanbellum@gmail.com>
2018-06-18 16:00:09 +0300

    method changed to use relation queries to get filtered user list for admin user

--- app/src/main/java/ru/supernacho/overtime/model/repository/EmployeeRepository.java
+++ app/src/main/java/ru/supernacho/overtime/model/repository/EmployeeRepository.java
@@ -24,20 +24,35 @@ public class EmployeeRepository {
     public Observable<Boolean> getEmployees() {
         return Observable.create(emit -> {
             employees.clear();
+            String[] activeCompanyId = new String[1];
+            ParseQuery<ParseObject> userQuery = ParseQuery.getQuery(ParseClass.USER_COMPANIES);
+            userQuery.whereEqualTo(ParseFields.userCompaniesUserId, ParseUser.getCurrentUser().getObjectId())
+                    .findInBackground((users, e1) -> {
+                        if (users != null && users.size() > 0) {
+                            activeCompanyId[0] = users.get(0).getString(ParseFields.userCompaniesActiveCompany);
 
-            ParseQuery<ParseObject> query = ParseQuery.getQuery(ParseClass.PARSE_USER);
-            query
-                    .whereEqualTo(ParseFields.userCompany, ParseUser.getCurrentUser().get(ParseFields.userCompany))
-                    .findInBackground((objects, e) -> {
-                        if (objects != null && e == null) {
-                            for (ParseObject object : objects) {
-                                object.getString(ParseFields.userId);
-                                employees.add(new User(object.getObjectId(), object.getString(ParseFields.userName), object.getString(ParseFields.fullName),
-                                        object.getString(ParseFields.userEmail)));
-                            }
-                            emit.onNext(true);
-                        } else {
-                            emit.onNext(false);
+                            ParseQuery<ParseObject> filterForCompany = ParseQuery.getQuery(ParseClass.USER_COMPANIES);
+                            filterForCompany.whereEqualTo(ParseFields.userCompaniesCompanies,
+                                    activeCompanyId[0]);
+
+                            ParseQuery<ParseObject> query = ParseQuery.getQuery(ParseClass.PARSE_USER);
+                            query
+                                    .whereMatchesKeyInQuery(ParseFields.userId, ParseFields.userCompaniesUserId,
+                                            filterForCompany)
+                                    .findInBackground((objects, e) -> {
+                                        if (objects != null && e == null) {
+                                            for (ParseObject object : objects) {
+                                                object.getString(ParseFields.userId);
+                                                employees.add(new User(object.getObjectId(),
+                                                        object.getString(ParseFields.userName),
+                                                        object.getString(ParseFields.fullName),
+                                                        object.getString(ParseFields.userEmail)));
+                                            }
+                                            emit.onNext(true);
+                                        } else {
+                                            emit.onNext(false);
+                                        }
+                                    });
                         }
                     });
         });
