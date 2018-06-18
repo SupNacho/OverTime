54047c6f48967ba61965b6d1a32e08fe55905b0c
Artem Shumilov <vanbellum@gmail.com>
2018-06-18 15:59:00 +0300

    added method for new UserCompanies class that doing user able can be in many companies and switch it on needed

--- app/src/main/java/ru/supernacho/overtime/model/repository/LoginRepository.java
+++ app/src/main/java/ru/supernacho/overtime/model/repository/LoginRepository.java
@@ -1,12 +1,14 @@
 package ru.supernacho.overtime.model.repository;
 
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import io.reactivex.Observable;
 import io.reactivex.subjects.PublishSubject;
 import ru.supernacho.overtime.model.Entity.User;
+import ru.supernacho.overtime.model.Entity.UserCompany;
 import ru.supernacho.overtime.utils.NetworkStatus;
-import timber.log.Timber;
 
 public class LoginRepository {
     private PublishSubject<RepoEvents> repoEventBus = PublishSubject.create();
@@ -37,7 +39,7 @@ public class LoginRepository {
     }
 
     public void loginIn(String userName, String password) {
-        if (NetworkStatus.getStatus() == NetworkStatus.Status.OFFLINE){
+        if (NetworkStatus.getStatus() == NetworkStatus.Status.OFFLINE) {
             repoEventBus.onNext(RepoEvents.LOGIN_SUCCESS);
         } else {
             ParseUser.logInInBackground(userName, password, (user, e) -> {
@@ -48,6 +50,22 @@ public class LoginRepository {
                 }
             });
         }
+    }
+
+    public Observable<Boolean> addCompanyToUser() {
+        return Observable.create(e -> {
+            ParseObject companies = new ParseObject(ParseClass.USER_COMPANIES);
+            companies.put(ParseFields.userCompaniesUserId, ParseUser.getCurrentUser().getObjectId());
+            companies.put(ParseFields.userCompaniesActiveCompany, ParseUser.getCurrentUser().get(ParseFields.userCompany));
+            companies.addUnique(ParseFields.userCompaniesCompanies, ParseUser.getCurrentUser().get(ParseFields.userCompany));
+            companies.saveEventually(e1 -> {
+                if (e1 == null) {
+                    e.onNext(true);
+                } else {
+                    e.onNext(false);
+                }
+            });
+        });
     }
 
     public Observable<Boolean> logout() {
@@ -77,10 +95,30 @@ public class LoginRepository {
             emit.onNext(username);
         });
     }
-    public Observable<Boolean> userIsAdmin() {
+
+    public Observable<UserCompany> userIsAdmin() {
         return Observable.create(emit -> {
-            boolean isAdmin = ParseUser.getCurrentUser().getBoolean(ParseFields.isAdmin);
-            emit.onNext(isAdmin);
+            ParseQuery<ParseObject> userCompaniesQuery = ParseQuery.getQuery(ParseClass.USER_COMPANIES);
+            userCompaniesQuery.whereEqualTo(ParseFields.userCompaniesUserId, ParseUser.getCurrentUser().getObjectId())
+                    .findInBackground((objects, e) -> {
+                        if (objects != null && objects.size() > 0) {
+                            String companyId = null;
+                            for (ParseObject object : objects) {
+                                companyId = object.getString(ParseFields.userCompaniesActiveCompany);
+                            }
+                            ParseQuery<ParseObject> companyQuery = ParseQuery.getQuery(ParseClass.COMPANY);
+                            String finalCompanyId = companyId;
+                            companyQuery.whereEqualTo(ParseFields.companyId, companyId)
+                                    .whereEqualTo(ParseFields.companyAdmins, ParseUser.getCurrentUser().getObjectId())
+                                    .findInBackground((objects1, e1) -> {
+                                        if (objects1 != null && objects1.size() > 0) {
+                                            emit.onNext(new UserCompany(finalCompanyId, true));
+                                        } else {
+                                            emit.onNext(new UserCompany(finalCompanyId, false));
+                                        }
+                                    });
+                        }
+                    });
         });
     }
 
