56cb8dd13776f1d26d649d98cac50030800dd538
Artem Shumilov <vanbellum@gmail.com>
2018-06-19 12:45:37 +0300

    MvpView for Company Info

--- app/src/main/java/ru/supernacho/overtime/view/fragments/CompanyInfoView.java
+++ app/src/main/java/ru/supernacho/overtime/view/fragments/CompanyInfoView.java
@@ -1,4 +1,15 @@
 package ru.supernacho.overtime.view.fragments;
 
-public class CompanyInfoView {
+import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.SingleStateStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
+
+@StateStrategyType(value = SingleStateStrategy.class)
+public interface CompanyInfoView extends MvpView {
+    void setName(String name);
+    void setEmail(String email);
+    void setPhone(String phone);
+    void setAddress(String address);
+    void setPin(String pin);
+    void setCEO(String ceo);
 }
