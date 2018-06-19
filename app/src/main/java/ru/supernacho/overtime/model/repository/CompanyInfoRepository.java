7c5fe56b83e354db949af1b58304d486478a9a14
Artem Shumilov <vanbellum@gmail.com>
2018-06-19 12:45:08 +0300

    Added repository and its module

--- app/src/main/java/ru/supernacho/overtime/model/repository/CompanyInfoRepository.java
+++ app/src/main/java/ru/supernacho/overtime/model/repository/CompanyInfoRepository.java
@@ -1,4 +1,31 @@
 package ru.supernacho.overtime.model.repository;
 
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import io.reactivex.Observable;
+import ru.supernacho.overtime.model.Entity.CompanyEntity;
+
 public class CompanyInfoRepository {
+    public Observable<CompanyEntity> getCompanyInfo(){
+        return Observable.create( emit ->{
+            ParseQuery<ParseObject> userCompaniesQuery = ParseQuery.getQuery(ParseClass.USER_COMPANIES);
+            userCompaniesQuery.whereEqualTo(ParseFields.userCompaniesUserId, ParseUser.getCurrentUser().getObjectId());
+
+            ParseQuery<ParseObject> companyQuery = ParseQuery.getQuery(ParseClass.COMPANY);
+            ParseObject company = companyQuery.whereMatchesKeyInQuery(ParseFields.companyId, ParseFields.userCompaniesActiveCompany, userCompaniesQuery)
+                    .getFirst();
+            if (company != null) {
+                emit.onNext(new CompanyEntity(
+                        company.getString(ParseFields.companyName),
+                        company.getString(ParseFields.companyAddress),
+                        company.getString(ParseFields.companyPhone),
+                        company.getString(ParseFields.companyEmail),
+                        company.getString(ParseFields.companyChief),
+                        company.getString(ParseFields.companyEmpPin)
+                ));
+            }
+        });
+    }
 }
