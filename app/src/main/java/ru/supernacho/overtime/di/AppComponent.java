aea8026289c3d19aaac819dbc92a05bd347fd21b
Artem Shumilov <vanbellum@gmail.com>
2018-06-19 12:43:53 +0300

    added module for dialog fragment

--- app/src/main/java/ru/supernacho/overtime/di/AppComponent.java
+++ app/src/main/java/ru/supernacho/overtime/di/AppComponent.java
@@ -5,6 +5,7 @@ import javax.inject.Singleton;
 import dagger.Component;
 import ru.supernacho.overtime.di.modules.AppModule;
 import ru.supernacho.overtime.di.modules.ChartRepoModule;
+import ru.supernacho.overtime.di.modules.CompanyInfoRepoModule;
 import ru.supernacho.overtime.di.modules.CompanyRepoModule;
 import ru.supernacho.overtime.di.modules.EmploeeRepoModule;
 import ru.supernacho.overtime.di.modules.LogRepoModule;
@@ -14,6 +15,7 @@ import ru.supernacho.overtime.model.repository.CompanyRepository;
 import ru.supernacho.overtime.model.repository.LoginRepository;
 import ru.supernacho.overtime.model.repository.TimerRepository;
 import ru.supernacho.overtime.presenter.ChartPresenter;
+import ru.supernacho.overtime.presenter.CompanyInfoPresenter;
 import ru.supernacho.overtime.presenter.CompanyRegistrationPresenter;
 import ru.supernacho.overtime.presenter.DateChooserPresenter;
 import ru.supernacho.overtime.presenter.EmployeesPresenter;
@@ -25,7 +27,7 @@ import ru.supernacho.overtime.view.LoginActivity;
 
 @Singleton
 @Component(modules = {AppModule.class, LoginRepoModule.class, TimerRepoModule.class, LogRepoModule.class,
-        ChartRepoModule.class, EmploeeRepoModule.class, CompanyRepoModule.class})
+        ChartRepoModule.class, EmploeeRepoModule.class, CompanyRepoModule.class, CompanyInfoRepoModule.class})
 public interface AppComponent {
     void inject(LoginActivity activity);
     void inject(LoginPresenter presenter);
@@ -36,4 +38,5 @@ public interface AppComponent {
     void inject(ManagerPresenter presenter);
     void inject(EmployeesPresenter presenter);
     void inject(CompanyRegistrationPresenter presenter);
+    void inject(CompanyInfoPresenter presenter);
 }
