8c6c375046afa3a6259756be1f38c77ab0d5ecae
Artem Shumilov <vanbellum@gmail.com>
2018-06-19 12:41:21 +0300

    added files for company info dialog fragment

---
+++ app/src/main/java/ru/supernacho/overtime/model/Entity/CompanyEntity.java
@@ -0,0 +1,4 @@
+package ru.supernacho.overtime.model.Entity;
+
+public class CompanyEntity {
+}
---
+++ app/src/main/java/ru/supernacho/overtime/model/repository/CompanyInfoRepository.java
@@ -0,0 +1,4 @@
+package ru.supernacho.overtime.model.repository;
+
+public class CompanyInfoRepository {
+}
