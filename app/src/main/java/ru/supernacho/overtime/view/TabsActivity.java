fdfcee5170cec1ef39e37a45cb125793c6a8e599
Artem Shumilov <vanbellum@gmail.com>
2018-06-20 15:36:40 +0300

    added code to menu item to open dialog fragment

--- app/src/main/java/ru/supernacho/overtime/view/TabsActivity.java
+++ app/src/main/java/ru/supernacho/overtime/view/TabsActivity.java
@@ -27,6 +27,7 @@ import ru.supernacho.overtime.presenter.TabsPresenter;
 import ru.supernacho.overtime.view.adapters.FragmentAdapter;
 import ru.supernacho.overtime.view.custom.KeyboardStateListener;
 import ru.supernacho.overtime.view.custom.SoftKeyboardCoordinatorLayout;
+import ru.supernacho.overtime.view.fragments.ChooseCompanyFragment;
 import ru.supernacho.overtime.view.fragments.CompanyInfoFragment;
 import ru.supernacho.overtime.view.fragments.FragmentTag;
 import ru.supernacho.overtime.view.fragments.LogsFragment;
@@ -206,6 +207,8 @@ public class TabsActivity extends MvpAppCompatActivity implements TabsView {
             case R.id.action_employee_management:
                 return true;
             case R.id.action_choose_company:
+                ChooseCompanyFragment chooseCompanyFragment = new ChooseCompanyFragment();
+                chooseCompanyFragment.show(getSupportFragmentManager(), FragmentTag.COMPANY_CHOOSE_DIALOG);
                 return true;
             case R.id.action_reg_company:
                 return true;
